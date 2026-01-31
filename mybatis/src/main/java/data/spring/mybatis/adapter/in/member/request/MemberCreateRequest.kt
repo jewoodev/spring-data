97a4363d83c3329f8b1e764d633072233514acc1
jewoodev <jewoos15@naver.com>
2026-02-01 01:01:32 +0900

    feat: 회원 컨트롤러, 회원가입 API

---
+++ mybatis/src/main/java/data/spring/mybatis/adapter/in/member/request/MemberCreateRequest.kt
@@ -0,0 +1,29 @@
+package data.spring.mybatis.adapter.`in`.member.request
+
+import data.spring.mybatis.domain.member.Member
+import data.spring.mybatis.domain.member.Role.UNVERIFIED
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Size
+
+data class MemberCreateRequest(
+    @field:NotNull
+    @field:Size(min = 2, max = 100, message = "아이디는 5자 이상 100자 이하로 입력해주세요.")
+    val username: String,
+
+    @field:NotNull
+    @field:Size(min = 8, max = 100, message = "비밀번호는 8자 이상 100자 이하로 입력해주세요.")
+    val password: String,
+
+    @field:Email
+    val email: String
+) {
+    fun toDomain(): Member {
+        return Member(
+            username = username,
+            password = password,
+            email = email,
+            role = UNVERIFIED
+        )
+    }
+}
