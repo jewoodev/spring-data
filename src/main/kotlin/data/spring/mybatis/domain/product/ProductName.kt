037a2f2ee8d63c20e5b2e0b4567ff26b24b6c1d7
jewoodev <jewoos15@naver.com>
2026-02-15 12:05:43 +0900

    refactor: 상품 이름, 생성 비용이 비싼 Regex를 정적 프로퍼티로 변경

--- src/main/kotlin/data/spring/mybatis/domain/product/ProductName.kt
+++ src/main/kotlin/data/spring/mybatis/domain/product/ProductName.kt
@@ -4,6 +4,10 @@ package data.spring.mybatis.domain.product
 value class ProductName(val value: String) {
     init {
         require(value.length in 2..100) { "상품명은 2~100자여야 합니다." }
-        require(value.matches(Regex("^[a-zA-Z0-9가-힣\\s]+$"))) { "상품명은 영어, 숫자, 한글, 공백만 허용됩니다." }
+        require(value.matches(nameRegex)) { "상품명은 영어, 숫자, 한글, 공백만 허용됩니다." }
+    }
+
+    companion object {
+        val nameRegex = Regex("^[a-zA-Z0-9가-힣\\s]+$")
     }
 }
